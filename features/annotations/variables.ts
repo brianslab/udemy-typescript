33bf089e04ffea3577cdebb2d11fd0db74757998
brian <bdkuhn602@gmail.com>
2022-08-02 00:34:53 -0700

    arrays, classes, and object literals

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -7,3 +7,18 @@ let nothing: undefined = undefined;
 
 // built-in objects
 let now: Date = new Date();
+
+// arrays
+let colors: string[] = [ 'red', 'green', 'blue' ];
+let numbers: number[] = [ 1, 2, 3 ];
+let truths: boolean[] = [ true, true, false ];
+
+// classes
+class Car {}
+let car: Car = new Car();
+
+// object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
