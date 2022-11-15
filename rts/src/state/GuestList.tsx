4e3fcc3332ac034d6d8d93a2a04c3b5dcd7d133b
Brian Kuhn <bdkuhn602@gmail.com>
2022-11-15 02:13:43 -0800

    state

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 const GuestList: React.FC = () => {
-  return <div>GuestList</div>;
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <h3>Guest List</h3>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button>Add Guest</button>
+    </div>
+  );
 };
 
 export default GuestList;
