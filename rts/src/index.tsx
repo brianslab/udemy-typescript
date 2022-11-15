60e60e069811ab7811a499b6e5295f4a64f0d448
Brian Kuhn <bdkuhn602@gmail.com>
2022-11-15 02:27:00 -0800

    start UserSearch example

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,9 +1,9 @@
 // 1) Import ReactDOM library
-import ReactDOM from "react-dom/client";
-import GuestList from "./state/GuestList";
+import ReactDOM from 'react-dom/client';
+import UserSearch from './state/UserSearch';
 
 // 2) Get a reference to the div with ID root
-const el = document.getElementById("root");
+const el = document.getElementById('root');
 
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el!);
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <GuestList />
+      <UserSearch />
     </div>
   );
 };
