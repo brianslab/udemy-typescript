46ea4ff08b9e7d1c9b94427643abeb0700ab4420
Brian Kuhn <bdkuhn602@gmail.com>
2022-11-15 02:42:58 -0800

    type unions

--- rts/src/state/UserSearch.tsx
+++ rts/src/state/UserSearch.tsx
@@ -1,7 +1,32 @@
 import { useState } from 'react';
 
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 21 },
+  { name: 'Michael', age: 22 },
+];
+
 const UserSearch: React.FC = () => {
-  return <div>UserSearch</div>;
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input value={name} onChange={(e) => setName(e.target.value)}></input>
+      <button onClick={onClick}>Find User</button>
+      <div>{user && user.name}</div>
+      <div>{user && user.age}</div>
+    </div>
+  );
 };
 
 export default UserSearch;
