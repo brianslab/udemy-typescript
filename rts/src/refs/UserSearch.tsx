571800628572e7a8f4199162aa78db13925959bc
Brian Kuhn <bdkuhn602@gmail.com>
2022-11-16 03:16:49 -0800

    apply types to refs

---
+++ rts/src/refs/UserSearch.tsx
@@ -0,0 +1,37 @@
+import { useState, useRef } from 'react';
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 21 },
+  { name: 'Michael', age: 22 },
+];
+
+const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User Search
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <button onClick={onClick}>Find User</button>
+      <div>{user && user.name}</div>
+      <div>{user && user.age}</div>
+    </div>
+  );
+};
+
+export default UserSearch;
