9b5504f6fe2cc3a38ad4309c4430a2181393f9b4
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-09 17:18:20 -0800

    transpiling working

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import * as esbuild from 'esbuild-wasm';
 
@@ -6,21 +6,30 @@ const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
+  const ref = useRef<any>();
+
   const startService = async () => {
-    const service = await esbuild.startService({
+    ref.current = await esbuild.startService({
       worker: true,
       wasmURL: '/esbuild.wasm',
     });
-
-    console.log(service);
   };
 
   useEffect(() => {
     startService();
   }, []);
 
-  const onClick = () => {
-    console.log(input);
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
   };
 
   return (
