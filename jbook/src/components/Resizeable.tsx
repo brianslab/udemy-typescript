835dd8dbb4a3f0356c19ed76f05238b83559b8d4
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-12 23:01:42 -0800

    implement horizontal resize

--- jbook/src/components/Resizeable.tsx
+++ jbook/src/components/Resizeable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 import './Resizeable.css';
 
@@ -8,16 +8,27 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox
-      height={300}
-      width={Infinity}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      resizeHandles={['s']}
-    >
-      {children}
-    </ResizableBox>
-  );
+  let resizeableProps: ResizableBoxProps;
+  if (direction === 'horizontal') {
+    resizeableProps = {
+      className: 'resize-horizontal',
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      resizeHandles: ['e'],
+    };
+  } else {
+    resizeableProps = {
+      height: 300,
+      width: Infinity,
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizeableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
