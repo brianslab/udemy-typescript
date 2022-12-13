8d152f8aa63ca90d7450aea15dcd8e4c1777dc15
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-12 22:44:08 -0800

    constrain vertical resize

--- jbook/src/components/Resizeable.tsx
+++ jbook/src/components/Resizeable.tsx
@@ -9,7 +9,12 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   return (
-    <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
+    <ResizableBox
+      height={300}
+      width={Infinity}
+      maxConstraints={[Infinity, window.innerHeight * 0.9]}
+      resizeHandles={['s']}
+    >
       {children}
     </ResizableBox>
   );
