a7b6315ac06d4b74dbfa3d19cd85d8c5f0182a4d
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-13 04:08:13 -0800

    select ordered list

--- jbook/src/components/CellList.tsx
+++ jbook/src/components/CellList.tsx
@@ -1,4 +1,10 @@
+import { useTypedSelector } from '../hooks/useTypedSelector';
+
 const CellList: React.FC = () => {
+  const cells = useTypedSelector(({ cells: { order, data } }) =>
+    order.map((id) => data[id])
+  );
+
   return <div>Cell List</div>;
 };
 
