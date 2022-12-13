72bba0fa8486fc6afce9d89fbd88f75570f0bbb0
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-13 03:54:08 -0800

    undo testing actions

--- jbook/src/state/store.ts
+++ jbook/src/state/store.ts
@@ -2,24 +2,5 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import reducers from './reducers';
-import { ActionType } from './action_types';
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: 'javascript',
-  },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: 'text',
-  },
-});
-
-console.log(store.getState());
