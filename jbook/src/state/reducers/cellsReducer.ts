cec8f8f478734814a1babbce48a3d6739ff67107
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-13 03:18:23 -0800

    move cell logic

--- jbook/src/state/reducers/cellsReducer.ts
+++ jbook/src/state/reducers/cellsReducer.ts
@@ -24,16 +24,30 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
+
       state.data[id].content = content;
+
       return;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
+
       return;
     case ActionType.INSERT_CELL_BEFORE:
       return state;
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
+      return;
     default:
       return state;
   }
