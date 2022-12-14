c609079b1c755c57d0fab027c01b79f78c0e1d89
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-13 23:08:28 -0800

    extract input local state

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -4,15 +4,21 @@ import Resizable from './Resizeable';
 import CodeEditor from './CodeEditor';
 import Preview from './Preview';
 import bundler from '../bundler';
+import { Cell } from '../state';
+import { useActions } from '../hooks/useActions';
 
-const CodeCell = () => {
-  const [input, setInput] = useState('');
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState('');
   const [err, setErr] = useState('');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundler(input);
+      const output = await bundler(cell.content);
       setCode(output.code);
       setErr(output.err);
     }, 1000);
@@ -20,15 +26,15 @@ const CodeCell = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction='vertical'>
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction='horizontal'>
           <CodeEditor
-            initialValue='console.log("Hello, World!");'
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} bundlerStatus={err} />
