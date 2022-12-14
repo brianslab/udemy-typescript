e51e7fc0afee4b33a481584fbdacc6609908eb47
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-14 03:51:12 -0800

    begin bundles reducer

--- jbook/src/state/reducers/bundlesReducer.ts
+++ jbook/src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,31 @@
-export default 1;
+import produce from 'immer';
+import { ActionType } from '../action_types';
+import { Action } from '../actions';
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundlesState = {};
+
+const reducer = produce(
+  (state: BundlesState = initialState, action: Action): BundlesState => {
+    switch (action.type) {
+      case ActionType.BUNDLE_START: {
+        return state;
+      }
+      case ActionType.BUNDLE_COMPLETE: {
+        return state;
+      }
+      default:
+        return state;
+    }
+  },
+  initialState
+);
+
+export default reducer;
