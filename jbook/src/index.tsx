4b0447cbcf5aa831bf4bddae90f22c9dded0987f
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-13 23:36:55 -0800

    add icons to button

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -2,9 +2,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// import CodeCell from './components/CodeCell';
-// import TextEditor from './components/TextEditor';
 import CellList from './components/CellList';
 
 const App = () => {
