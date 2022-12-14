72c88762283329de3c936b0c6aeb1325098376cd
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-14 00:07:59 -0800

    begin addcell component

---
+++ jbook/src/components/AddCell.tsx
@@ -0,0 +1,21 @@
+import { useActions } from '../hooks/useActions';
+import './AddCell.css';
+
+interface AddCellProps {
+  nextCellID: string;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellID }) => {
+  const { insertCellBefore } = useActions();
+
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellID, 'javascript')}>
+        Code
+      </button>
+      <button onClick={() => insertCellBefore(nextCellID, 'text')}>Text</button>
+    </div>
+  );
+};
+
+export default AddCell;
