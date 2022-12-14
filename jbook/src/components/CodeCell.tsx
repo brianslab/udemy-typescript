0b9a0d7a5f8ce7eac33edf18e5e12ea4eb14e545
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-14 05:14:29 -0800

    fix iframe flickering

--- jbook/src/components/CodeCell.tsx
+++ jbook/src/components/CodeCell.tsx
@@ -50,15 +50,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {!bundle || bundle.loading ? (
-          <div className='progress-cover'>
-            <progress className='progress is-small is-primary' max='100'>
-              Loading
-            </progress>
-          </div>
-        ) : (
-          bundle && <Preview code={bundle.code} bundlerStatus={bundle.err} />
-        )}
+        <div className='progress-wrapper'>
+          {!bundle || bundle.loading ? (
+            <div className='progress-cover'>
+              <progress className='progress is-small is-primary' max='100'>
+                Loading
+              </progress>
+            </div>
+          ) : (
+            bundle && <Preview code={bundle.code} bundlerStatus={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
