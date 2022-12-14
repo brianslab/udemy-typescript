c676ab6be26af18075c2503bab452ba8404da9ec
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-14 00:29:32 -0800

    style buttons

--- jbook/src/components/AddCell.tsx
+++ jbook/src/components/AddCell.tsx
@@ -11,11 +11,23 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellID }) => {
   return (
     <div className='add-cell'>
       <div className='add-buttons'>
-        <button onClick={() => insertCellBefore(nextCellID, 'javascript')}>
-          Code
+        <button
+          className='button is-rounded is-primary is-small'
+          onClick={() => insertCellBefore(nextCellID, 'javascript')}
+        >
+          <span className='icon is-small'>
+            <i className='fas fa-plus' />
+          </span>
+          <span>Code</span>
         </button>
-        <button onClick={() => insertCellBefore(nextCellID, 'text')}>
-          Text
+        <button
+          className='button is-rounded is-primary is-small'
+          onClick={() => insertCellBefore(nextCellID, 'text')}
+        >
+          <span className='icon is-small'>
+            <i className='fas fa-plus' />
+          </span>
+          <span>Text</span>
         </button>
       </div>
       <div className='divider' />
